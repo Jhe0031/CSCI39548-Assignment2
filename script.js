7ad7ad481cd802ff94a6ff1cd0a6590a0328afb0
Sumiya Akter <sakter9012@gmail.com>
2021-02-24 15:34:16 -0500

    Update script.js
    Changed up some code, maybe it works better

--- script.js
+++ script.js
@@ -20,7 +20,15 @@ Array.prototype.myEach = function (callback) {
 
 /** (Sumiya) Without using the native “Array.prototype.map” method of JavaScript, compose a function titled “myMap” that 
     will take in an array of elements and execute a callback function on each of those elements. */
-Array.prototype.myMap = function (callback) {};
+Array.prototype.myMap = function (callback) 
+{
+    const newArray = [];
+    for (let i = 0; i < this.length; i++)
+    {
+        callback(this[i], i, this)
+    }
+    return newArray;
+} //CONFUSION
 
 /** (Li) Without using the native “Array.prototype.filter” method of JavaScript, compose a function titled “myFilter” 
     that will take in an array of elements and execute a callback function on each of those elements. */
@@ -33,7 +41,18 @@ Array.prototype.mySome = function(callback) {};
 
 /** (Sumiya) Without using the native “Array.prototype.every” method of JavaScript, compose a function titled “myEvery” 
     that will take in an array of elements and execute a callback function on each of those elements. */
-Array.prototype.myEvery = function(callback) {};
+Array.prototype.myEvery = function(callback) 
+{
+    for (let i = 0; i < this.length; i++)
+    {
+        callback(this[i], i, this);
+        if (i == true)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
 /** (Li) Without using the native “Array.prototype.reduce” method of JavaScript, compose a function titled “myReduce” 
     that will take in an array of elements and execute a callback function on each of those elements. */
@@ -47,7 +66,23 @@ Array.prototype.myIncludes = function(callback) {};
 /** (Sumiya) Without using the native “Array.prototype.indexOf” method of JavaScript, compose a function titled 
     “myIndexOf” that will take in an array of elements and returns the index of the first encounter of a target 
     element (if it is found) or -1 if that element does not exist within the input array. */
-Array.prototype.myIndexOf = function(callback) {};
+Array.prototype.myIndexOf = function(callback) 
+{
+    var element;
+    for (let i = 0; i < this.length; i++)
+    {
+        callback(element, 0);
+    }
+    if (element == true)
+    {
+        return i;
+    }
+    else
+    {
+        return -1;
+    }
+
+}
 
 /** (Li) Without using the native “Array.prototype.push” method of JavaScript, compose a function titled “myPush” 
     that will take in an array of elements as well as an elementToAdd and append that element to the end of the 
@@ -62,11 +97,20 @@ Array.prototype.myLastIndexOf = function(callback) {};
 /** (Sumiya) Object.keys() */
 /** Without using the native “Object.keys()” method of JavaScript, compose a function titled “grabKeys” that 
     will take in an object and return all of the keys of the key:value pairs of that object. */
-
+Array.prototype.grabKeys = function(callback)
+{
+    for (let i = 0; i < this.length; i++)
+    {
+        return i;
+    }
+}
 /** (Li) Object.values() */
 /** Without using the native “Object.values()” method of JavaScript, compose a function titled “grabValues” 
     that will take in an object and return all of the values of the key:value pairs of that object. */
+Array.prototype.grabValues = function(callback)
+{
 
+}
 
 /** -------------------------------------------------------------
     TESTING
